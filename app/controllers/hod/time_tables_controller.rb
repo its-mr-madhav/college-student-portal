dd3f3d1ecec51f6d90bc2ef0f304df660111c63a
mohdsameer <mohdsameer1310@gmail.com>
2019-04-20 16:04:53 +0530

    Deleted Previous Controllers and views

--- app/controllers/hod/time_tables_controller.rb
+++ app/controllers/hod/time_tables_controller.rb
@@ -1,2 +1,49 @@
 class Hod::TimeTablesController < Hod::MainController
+  before_action :get_time_table, only: [:edit, :update, :destroy]
+
+  def index
+    @time_tables = TimeTable.all
+  end
+
+  def create
+    time_table = TimeTable.new(time_table_params)
+    time_table.save
+    redirect_to time_tables_path
+  end
+
+  def edit
+  end
+
+  def new
+    @time_table = TimeTable.new
+  end
+
+  def show
+  end
+
+  def destroy
+    if @time_table.destroy
+      redirect_to root_url, notice: "Time Table deleted."
+    else
+      redirect_to time_tables_path, alert: "Try Again!!!"
+    end
+  end
+
+  def update
+    if @time_table.update(time_table_params)
+      redirect_to time_tables_path, notice: "Time Table is updated successfully"
+    else
+      render action: "edit"
+    end
+  end
+
+  private
+
+  def time_table_params
+    params.require(:time_table).permit(:faculty_id, :faculty_subject_id)
+  end
+
+  def time_table
+    @time_table = TimeTable.find(params[:id])
+  end
 end
--- test/controllers/faculty_subjects_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FacultySubjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
